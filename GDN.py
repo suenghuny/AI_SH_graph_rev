97c48f6d001a14955da31bcfcc2727a6140e9d09
suenghun <63846187+suenghun@users.noreply.github.com>
2023-06-14 21:23:38 +0900

    temp_constant 추가

--- GDN.py
+++ GDN.py
@@ -1026,7 +1026,7 @@ class Agent:
                 action.append(u)
         else:
             if step != None:
-                temperature = (step + 1) / 100
+                temperature = (step + 1) / 100*cfg.temp_constant
                 if temperature <=2000:
                     Q = Q*temperature
 
